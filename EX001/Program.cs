4096600dcad4b06bbb304211d8ada58b64ee4e7a
Kristina Belova <belova44893@gmail.com>
2022-11-21 22:03:18 +0200

    add ex001

---
+++ EX001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine($"{numA} больше, чем {numB}");
+}
+else 
+{
+    Console.WriteLine($"{numB} больше, чем {numA}");
+}
