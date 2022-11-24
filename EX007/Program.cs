514c631e885da72adfa3964389a3b93981c8bee7
Kristina Belova <belova44893@gmail.com>
2022-11-24 22:55:21 +0200

    add ex007

---
+++ EX007/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным
+
+Console.Clear();
+
+Console.WriteLine("Введите номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5)
+{
+    Console.WriteLine("Работаем((((");
+}
+else if (num == 6 || num == 7)
+{
+    Console.WriteLine("Выходной!!!");
+}
+
+else
+    Console.WriteLine("Это не день недели!");
