41b572b6b47656102008a247d3c3c8b5faf9b134
Kristina Belova <belova44893@gmail.com>
2022-11-24 22:47:02 +0200

    add ex006

---
+++ EX006/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+
+Console.Clear();
+
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
+
+Console.WriteLine("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int str = num.ToString().Length;
+
+Console.Write(MakeArray(num, str));
+
+
+    
+
+
+
+
+
+
+
