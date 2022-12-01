daab591caa72608ab2f454a14e233b8ea34f835f
Kristina Belova <belova44893@gmail.com>
2022-12-01 22:47:54 +0200

    add ex013

---
+++ EX013/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int [size];
+
+int[] FillArray(int[] array)
+{
+    int length  = array.Length;
+    int index = 0;
+    while (index< length)
+    {
+        array[index] = new Random().Next(0,10000);
+        index++;
+    }
+    return array;
+}
+
+Console.WriteLine("[" + String.Join(",", FillArray(mass)) + "]");
