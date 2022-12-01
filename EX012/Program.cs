649ceb0b51edf04f4ebf4bf2ae6df2a75071434a
Kristina Belova <belova44893@gmail.com>
2022-12-01 22:34:23 +0200

    add ex012

---
+++ EX012/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear();
+
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр в числе {num} равна {FaindSumOfNums(num)} ");
+
+
+int FaindSumOfNums(int numbers)
+{
+    int result = 0;
+    int i = numbers;
+    
+    while (i>0){
+    int n = i % 10;
+    i = i /10;
+    result = result + n;
+    }
+    return result;
+}
