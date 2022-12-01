3b4bb252f7d2e625d5754d94b55b829e10ebf517
Kristina Belova <belova44893@gmail.com>
2022-12-02 00:17:24 +0200

    add ex015 with an asterisk

---
+++ EX015/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+/*int FaindMaxNum(int num)
+{
+    int max = 0;
+    for (int i = num; i > 0; i++)
+    {
+        int n = i % 10;
+        if (n > max) 
+            max = n;
+        i = i/10;
+    }
+
+    return max;
+
+}*/
+
+int num = 437;
+int max = 0;
+int n = 0; 
+
+while (num>0){
+    n = num % 10;
+    if (n > max)
+        max = n;
+    num = num / 10;
+}
+
+Console.Write(max);
