907de11fb4e5fd667422498c19c002dd2d60fedf
Kristina Belova <belova44893@gmail.com>
2022-12-01 22:10:26 +0200

    add ex011

---
+++ EX011/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+
+Console.Write("Введите первое число : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {num1} в степени {num2} = {RaisePowerNum(num1,num2)}");
+
+int RaisePowerNum(int a,int b){
+
+    int result = a;
+
+for(int i = 1; i < b; i++){
+    result = result * a;
+}
+    return result;
+}
+
