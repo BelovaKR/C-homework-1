34565ba178a6f21168482f25dd2badeaf7f549b2
Kristina Belova <belova44893@gmail.com>
2022-12-02 00:18:56 +0200

    add ex014 with an asterisk

---
+++ EX014/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая на вход принимает радиус 
+//круга и находит его площадь округленную до целого числа, 
+//необходимо вывести максимальную цифру в полученном округлённом 
+//значении площади круга.
+//10 -> 4
+//20 -> 7
+//30 -> 8
+
+Console.Clear();
+Console.WriteLine("Введите радиус круга: ");
+double R = Convert.ToDouble(Console.ReadLine());
+double Pi = 3.141592653589793;
+double S = 0;
+
+S = Pi * R * R;
+Console.WriteLine($"Площадь круга = {Math.Round(S, MidpointRounding.ToEven)}");
+
+int num = Convert.ToInt32(S);
+
+Console.WriteLine($"Максимальная цифра - {FaindMaxNum(num)}");
+
+int FaindMaxNum(int num)
+{
+    int max = 0;
+    int n = 0; 
+
+        while (num>0){
+            n = num % 10;
+            if (n > max)
+             max = n;
+        num = num / 10;
+        }
+     return max;
+}
+
