a44cc772cb19c6a338a9d7d0cfa66c546ebf80c3
Kristina Belova <belova44893@gmail.com>
2022-12-09 22:55:32 +0200

    edd ex018

---
+++ EX018/Program.cs
@@ -0,0 +1,40 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.WriteLine("Введите количество значений для ввода:");
+
+int numberValue = Convert.ToInt32(Console.ReadLine());
+
+int[] mas = new int [numberValue];
+
+int [] FillArrayKeyboard(int [] array)
+{
+    for(int i=0; i<array.Length;i++){
+        Console.Write($"Введите {i+1} значение: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    } 
+    return array;
+}
+Console.WriteLine($"Введенные значения :[" + String.Join(",", FillArrayKeyboard(mas)) + "]");
+
+int CheckNumMoreZero (int [] arr){
+    int count =0;
+    int index =0;
+    while (index < arr.Length)
+    {
+        if (arr[index]>0)
+        {
+            count = count +1;
+            index++;
+        }
+        else
+            index++;
+    }
+    return count;
+}
+
+Console.WriteLine($"Введено значений больше 0 --> {CheckNumMoreZero(mas)} ");
