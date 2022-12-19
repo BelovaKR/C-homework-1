aea49aaba2d6f32e910283a26fe0bb6a0682762e
Kristina Belova <belova44893@gmail.com>
2022-12-19 22:07:48 +0200

    add ex029 task #68

---
+++ EX029/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Задайте значения M и N. 
+// Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+// M = 28; N = 7 -> 7
+
+Console.Clear();
+Console.Write("Введите число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M = {m}, N = {n} наибольший общий делитель --> {FindDivider(m,n)}");
+
+int FindDivider(int a, int b)
+{
+    if (b == 0) return a;             // Закомментировала для себя.
+    return FindDivider(b, a % b);    // a = 28 ; b = 7 --> 28 % 7 = 0 --> return-7.
+                                     // a = 24 ; b = 54 --> 24 % 54 = 6 --> 6 % 6 = 0 --> return - 6.
+}
